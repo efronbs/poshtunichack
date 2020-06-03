87b44e70cc7a10e49a630311a7859a920470445a
Benjamin Efron <efronbs@rose-hulman.edu>
2020-06-03 12:38:08 -0700

    fix bugs with run script

--- webserver/run.sh
+++ webserver/run.sh
@@ -1,9 +1,9 @@
 #!/bin/#!/usr/bin/env bash
 
 if [[ "$#" -ne 2 ]]; then
-  echo "Usage: $0 <path to ssl key file> <path to ssl cert file>"
+  echo "Usage: $0 <path to ssl cert file> <path to ssl private key file>"
   exit 1
 fi
 
-export FLASK_ENV=assets.py
-sudo python -m flask run -h 0.0.0.0 -p 6969 --key=$1 --cert=$2
+export FLASK_APP="assets.py"
+python3 -m flask run -h 0.0.0.0 -p 6969 --cert=$1 --key=$2
