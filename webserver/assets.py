459611df3e42800c624a9f3d9e64a976bf36476a
Benjamin Efron <efronbs@rose-hulman.edu>
2020-06-03 12:49:54 -0700

    server js and html separately

--- webserver/assets.py
+++ webserver/assets.py
@@ -2,9 +2,13 @@ import flask
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/code/inject.js')
+@app.route('/html')
 def get_code():
-    return flask.url_for("static", "inject.js")
+    return app.send_static_filename("inject.html")
+
+@app.route('/js')
+def get_code():
+    return app.send_static_filename("inject.js")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6969)
