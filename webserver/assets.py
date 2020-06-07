6f70bf442dc867065d7c9c5c40c8d85c68011434
Benjamin Efron <efronbs@rose-hulman.edu>
2020-06-07 15:07:47 -0700

    fix duplicate endpoint functions

--- webserver/assets.py
+++ webserver/assets.py
@@ -3,11 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/html')
-def get_code():
+def get_html():
     return app.send_static_filename("inject.html")
 
 @app.route('/js')
-def get_code():
+def get_js():
     return app.send_static_filename("inject.js")
 
 if __name__ == '__main__':
