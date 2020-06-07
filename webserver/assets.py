b82498d855955f63cb21c38bb7afd5bf1961b590
Benjamin Efron <efronbs@rose-hulman.edu>
2020-06-07 15:09:26 -0700

    fix flask api call

--- webserver/assets.py
+++ webserver/assets.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/html')
 def get_html():
-    return app.send_static_filename("inject.html")
+    return app.send_static_file("inject.html")
 
 @app.route('/js')
 def get_js():
-    return app.send_static_filename("inject.js")
+    return app.send_static_file("inject.js")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6969)
